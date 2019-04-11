e9d97b1dc837e013502af45f50028121d5488288
nauval <novalahmad846@gmail.com>
2019-04-11 11:09:45 +0700

    Masuk PAK EKO!

---
+++ app/src/main/java/com/example/nauval/koperasipetik/KoperasiHome.java
@@ -0,0 +1,13 @@
+package com.example.nauval.koperasipetik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KoperasiHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koperasi_petik);
+    }
+}
